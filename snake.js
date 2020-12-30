5d224b976c19e5b943f802f9fa1d52f0e83099a9
aviva12 <snehagupta0701@gmail.com>
2020-12-30 15:11:33 +0530

    adding movement

--- snake.js
+++ snake.js
@@ -24,23 +24,30 @@ function init()
         drawSnake:function(){
             for(var i=0;i<this.cells.length;i++)
             {
+                pen.fillStyle="red";
                 pen.fillRect(this.cells[i].x*cs,this.cells[i].y,cs-2,cs);
             }
+        },
+        updateSnake:function(){
+            this.cells.pop();
+            
+            var headX=this.cells[0].x;
+            var headY=this.cells[0].y;
+            var X=headX+1;
+            var Y=headY;
+            this.cells.unshift({x:X,y:Y});
         }
     }
     snake.createSnake();
 }
 
 function draw(){
+    pen.clearRect(0,0,W,H);
     snake.drawSnake();
 }
 
 function update(){
-//    rect.x+=rect.speed;
-//    if(rect.x>W-rect.w||rect.x<0)
-//    {
-//        rect.speed*=-1;
-//    }
+    snake.updateSnake();
 }
 
 function gameloop(){
