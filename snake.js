f85c34994200126faf30250b6d77df6702fa93b6
aviva12 <snehagupta0701@gmail.com>
2020-12-30 14:58:28 +0530

    setting snake

--- snake.js
+++ snake.js
@@ -2,39 +2,48 @@
 //document object model
 //js can access any element present on the webpage by traversing this dom
 
-
 function init()
 {
     var canvas=document.getElementById('mycanvas');
-    W=canvas.width=500;
-    H=canvas.height=500;
+    W=canvas.width=600;
+    H=canvas.height=600;
     //canvas is used to draw graphics using javascript
     pen=canvas.getContext('2d');
-    rect={
-        x:20,
-        y:20,
-        w:50,
-        h:50,
-        speed:10,
+    cs=65;
+    snake={
+        init_len:5,
+        color:"blue",
+        cells:[],
+        direction:"right",
+        createSnake:function(){
+            for(var i=this.init_len;i>0;i--)
+            {
+                this.cells.push({x:i,y:0});
+            }
+        },
+        drawSnake:function(){
+            for(var i=0;i<this.cells.length;i++)
+            {
+                pen.fillRect(this.cells[i].x*cs,this.cells[i].y,cs-2,cs);
+            }
+        }
     }
+    snake.createSnake();
 }
 
 function draw(){
-    pen.clearRect(0,0,W,H);
-    pen.fillStyle="red";
-    pen.fillRect(rect.x,rect.y,rect.w,rect.h);
+    snake.drawSnake();
 }
 
 function update(){
-    rect.x+=rect.speed;
-    if(rect.x>W-rect.w||rect.x<0)
-    {
-        rect.speed*=-1;
-    }
+//    rect.x+=rect.speed;
+//    if(rect.x>W-rect.w||rect.x<0)
+//    {
+//        rect.speed*=-1;
+//    }
 }
 
 function gameloop(){
-    console.log("In game loop");
     draw();
     update();
 }
